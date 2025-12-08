4871369503700e538ce555f2b40601e6dafbc0d2
calvin mostly <calvin@ubuntu.laptop>
2025-12-08 11:05:37 -0800

    adding json

--- flag.py
+++ flag.py
@@ -1,23 +1,32 @@
-import numpy as np
-from PIL import Image as im
+from pgl import GWindow, GImage
+from ColorObj import ColorObj
 
 # For your convenience - 700 worked well for me
-IMAGE_SIZE = 700
+GWINDOW_SIZE = 700
 MAX_BRIGHTNESS = 255
-NUM_COLORS = 3
 
-def get_colors(img_arr, region):
-    return img_arr * MAX_BRIGHTNESS
+# For your convenience - the URL of rbgs.json
+URL = "https://cd-public.github.io/courses/cs1/exams/rgbs.json"
+
+gw = GWindow(GWINDOW_SIZE, GWINDOW_SIZE)
 
 # Main Function
 def flag(region):
-    # Create an image array array IMAGE_SIZE pixels by IMAGE_SIZE pixels by NUM_COLORS colors
-    img_arr = np.ones([IMAGE_SIZE,IMAGE_SIZE,NUM_COLORS]).astype(np.uint8)
-    # Set all colors in the image array
-    img_arr = get_colors(img_arr, region)
-    # Convert the array to an image
-    img = im.fromarray(img_arr)
-    # Save the image as "flag.png"
-    img.save("flag.png")
+    # This sample code creates a pixel array that is uninitialized
+    # TODO: Update the body of this function to create a flag
+    # Prob0: Implemement "read_rgbs" and update ColorObj.
+    # Prob1: I recommend calculations over the window size and the number of colors.
+    # Prob2: I recommend a dictionary of colors-to-ints that are incorporated into Prob0 calcs.
+    #        You will need to use the "region" argument for this problem.
+    # Prob3: Update ColorObj
+    # Prob4: Update ColorObj
+    rojo = ColorObj("rojo")
+    parr = [[rojo.get_color_at(row,col) for row in range(GWINDOW_SIZE)] for col in range(GWINDOW_SIZE)]
+    return parr
 
-flag('Qullasuyu')
+# Helper Function
+# Likely useful in Prob0
+def read_rgbs():
+    return
+
+gw.add(GImage(flag('Qullasuyu')))
