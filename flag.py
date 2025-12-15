315c3b8a5b8dc9aaf05b2177e977245650d60066
calvin <cd-public@users.noreply.github.com>
2025-12-15 10:24:16 -0800

    Refactor flag function to initialize ColorObj correctly

--- flag.py
+++ flag.py
@@ -20,7 +20,7 @@ def flag(region):
     #        You will need to use the "region" argument for this problem.
     # Prob3: Update ColorObj
     # Prob4: Update ColorObj
-    rojo = ColorObj(None)
+    rojo = ColorObj({})
     parr = [[rojo.get_color_at(row,col) for row in range(GWINDOW_SIZE)] for col in range(GWINDOW_SIZE)]
     return parr
 
@@ -30,4 +30,5 @@ def read_rgbs():
     return
 
 gw.add(GImage(flag('Qullasuyu')))
+
 
